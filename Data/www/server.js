ffa7d3ea2d89d49d403617ac8f753188347c2220
Unknown <pilougames4@gmail.com>
2017-01-21 17:22:11 +0100

    Updated interface

--- Data/www/server.js
+++ Data/www/server.js
@@ -1,6 +1,10 @@
 var http = require("http");
 var url = require("url");
 
+var ipAdress = '127.0.0.1';//'192.168.41.148';
+var port = 8008;
+
+
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
@@ -8,7 +12,7 @@ function start(route, handle) {
     route(handle, pathname, response, request);
   }
 
-  http.createServer(onRequest).listen(8008);
+  http.createServer(onRequest).listen(port, ipAdress);
   console.log("Démarrage du serveur.");
 }
 
